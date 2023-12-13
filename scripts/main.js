f57f9b1a61054e7cc2875750cad53bfbce2064db
Steven Ortega <stevencortega99@gmail.com>
2023-12-13 02:05:33 -0600

    Updated content. More portfolio style now.

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,17 @@ myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   const myClass = myTheme.getAttribute("class");
 
+
   // Change the picture
-  if (mySrc === "images/gundam.jpg") {
+  if (mySrc === "images/spot-tower-photoshoot.jpeg") {
     myImage.setAttribute("src", "images/baby-yoda-2.jpg");
+    document.getElementById("figcap").textContent = `Baby Yoda is awesome!`;
   } else if (mySrc === "images/baby-yoda-2.jpg") {
-    myImage.setAttribute("src", "images/baby-yoda.jpeg");
-  } else {
     myImage.setAttribute("src", "images/gundam.jpg");
+    document.getElementById("figcap").textContent = `Gundam is also super cool!`;
+  } else {
+    myImage.setAttribute("src", "images/spot-tower-photoshoot.jpeg");
+    document.getElementById("figcap").textContent = `Steven is wearing orange :)`;
   }
 
   // Change the theme
@@ -42,14 +46,15 @@ function setUserName() {
   if (!myName){
     alert("Invalid name");
   } else {
-    localStorage.setItem("name", myName);
+    sessionStorage.setItem("name", myName);
     myHeading.textContent = `Hi ${myName} do you like Nike and Baby Yoda?`;
+    document.getElementById("easteregg").hidden=false;
   }
 }
 
-if (localStorage.getItem("name")) {
-  const storedName = localStorage.getItem("name");
-  myHeading.textContent = `${storedName} likes Nike and Baby Yoda!`;
+if (sessionStorage.getItem("name")) {
+  const storedName = sessionStorage.getItem("name");
+  myHeading.textContent = `Welcome back ${storedName}!`;
 }
 
 myButton.onclick = () => {
